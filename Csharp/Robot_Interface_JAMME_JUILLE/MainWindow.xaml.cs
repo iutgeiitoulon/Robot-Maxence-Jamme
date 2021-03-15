820511400cd362c93ee1d52528a490d7cd880f94
Keenan <keenan.jacq@gmail.com>
2021-03-15 18:47:42 +0100

    fin seance 1

---
+++ Csharp/Robot_Interface_JAMME_JUILLE/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Robot_Interface_JAMME_JUILLE
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void SendMessage()
+        {
+            string msg = TextBoxEmission.Text;
+            TextBoxRéception.Text = TextBoxRéception.Text + "Reçu: " + msg;            
+            TextBoxEmission.Text = "";
+        }
+        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+            string msg = TextBoxEmission.Text;
+            TextBoxRéception.Text = TextBoxRéception.Text + "Reçu: " + msg + "\n";
+            TextBoxEmission.Text = "";
+        }
+
+        private void TextBoxEmission_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendMessage();
+            }
+        }
+    }
+}
